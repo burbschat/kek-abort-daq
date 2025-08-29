78c35684521b8383e0067f7b1b2be1a657d84198
Bela Urbschat <b.urbschat@tum.de>
2025-08-29 23:33:34 +0900

    Generate correct bitstream image

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -4,6 +4,10 @@ export PRJ_VERSION = 0x02020000
 # Include .XSA in image dir
 export GEN_XSA_IMAGE = 1
 
+# 7000 series SoC requires bin (not bit) image
+# export GEN_BIT_IMAGE = 0  # Maybe we can leave it
+export GEN_BIN_IMAGE = 1
+
 # Define release
 ifndef RELEASE
 export RELEASE = all
