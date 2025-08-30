e29e8d23f7ebfa3b09e577399ec96aa7b312cc8f
Bela Urbschat <b.urbschat@tum.de>
2025-08-30 15:23:16 +0900

    Add post_build hook to generate usable bitstream for 7000 SoC

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -4,9 +4,15 @@ export PRJ_VERSION = 0x02020000
 # Include .XSA in image dir
 export GEN_XSA_IMAGE = 1
 
-# 7000 series SoC requires bin (not bit) image
-# export GEN_BIT_IMAGE = 0  # Maybe we can leave it
-export GEN_BIN_IMAGE = 1
+# 7000 series SoC requires bin (not bit) image. BUT the recent Vivado versions
+# do not produce the correct (bit-swapped) file.
+# Thus we must run a command in `post_build.tcl` target specific script for
+# now. 
+# The below setting will actually not trigger generation of the .bin file but
+# just copy it to the images directory (it's generated anyways).
+# Also, as the post_build hook is run after creation and copying of all
+# bitstream files, we cannot just overwrite the present .bit file.
+# export GEN_BIN_IMAGE = 1  # Will give the wrong file...
 
 # Define release
 ifndef RELEASE
