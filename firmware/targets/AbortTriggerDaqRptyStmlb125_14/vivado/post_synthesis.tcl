c6db9afe671f1d675b084749818cb39e8022c26b
Bela Urbschat <b.urbschat@tum.de>
2026-07-22 14:31:49 +0900

    ILA debug without dma AXI Muxes

--- firmware/targets/AbortTriggerDaqRptyStmlb125_14/vivado/post_synthesis.tcl
+++ firmware/targets/AbortTriggerDaqRptyStmlb125_14/vivado/post_synthesis.tcl
@@ -43,9 +43,12 @@ SetDebugCoreClk ${ilaName} {U_App/axilClk}
 
 ConfigProbe ${ilaName} {U_App/axilRst}
 
+# AXI Stream ring buffer output
 ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axisMaster*}
 ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axisSlave*}
 
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/TX_FIFO/sAxisMaster*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/TX_FIFO/sAxisSlave*}
 
 # ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_ChanGen[0].U_DmaWriteMux/dataWriteMaster[*]*}
 # ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_ChanGen[0].U_DmaWriteMux/dataWriteSlave[*]*}
@@ -59,27 +62,62 @@ ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axisSlave*}
 # # ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_ChanGen[0].U_DmaWriteMux/mAxiWriteCtrl*}
 
 # Data written to ring buffer
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[ramWrData]*}
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[nextAddr]*}
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[ramWrEn]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[ramWrData]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[nextAddr]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/dataR[ramWrEn]*}
 
 # Data read from ring buffer
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[rdEn]*}
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[ramRdAddr]*}
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[rdEn]*}
-ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/ramRdData*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[rdEn]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[ramRdAddr]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[wordCnt]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/axilR[rdEn]*}
+# ConfigProbe ${ilaName} {U_App/U_AxiStreamRingBuffer/ramRdData*}
 
 # Interface to CPU
-ConfigProbe ${ilaName} {U_core/REAL_CPU.U_CPU/U_CPU/axi_dma_*}
+# ConfigProbe ${ilaName} {U_core/REAL_CPU.U_CPU/U_CPU/axi_dma_*}
 
 # Protocol convert in/out
 # ConfigProbe ${ilaName} {U_core/REAL_CPU.U_CPU/U_CPU/axi_protocol_convert_2/m_axi_*}
 # ConfigProbe ${ilaName} {U_core/REAL_CPU.U_CPU/U_CPU/axi_protocol_convert_2/s_axi_*}
 
-
-# DMA interrupt signal
+# # DMA interrupt signal
 ConfigProbe ${ilaName} {U_core/U_DMA/dmaIrq}
+#
+# # DMA Descriptor state (check if stuck in WAIT_S)
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/stateMirror*}
+# # Check IRQ assert conditions
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intReqCountMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/holdoffCompare*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/forceIntMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intSwAckReqMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intEnableMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intAckCountMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intCompValid*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/invalidCount*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intDiffValid*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/diffCnt*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intSwAckEn*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intReqEnMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intHoldoffCountMirror*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_V2Gen/U_DmaDesc/intHoldoffMirror*}
 
+# DMA AXI signals
+# With Read/Write path Mux
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_WritePathMux/sAxiWriteMasters[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_WritePathMux/sAxiWriteSlaves[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_WritePathMux/mAxiWriteMaster[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_WritePathMux/mAxiWriteSlave[*]*}
+#
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_ReadPathMux/sAxiReadMasters[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_ReadPathMux/sAxiReadSlaves[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_ReadPathMux/mAxiReadMaster[*]*}
+# ConfigProbe ${ilaName} {U_core/U_DMA/U_ReadPathMux/mAxiReadSlave[*]*}
+#
+# Without Read/Write path Mux
+ConfigProbe ${ilaName} {U_core/U_DMA/axiWriteMaster[*]*}
+ConfigProbe ${ilaName} {U_core/U_DMA/axiWriteSlave[*]*}
+ConfigProbe ${ilaName} {U_core/U_DMA/axiReadMaster[*]*}
+ConfigProbe ${ilaName} {U_core/U_DMA/axiReadSlave[*]*}
 
 ##########################
 ## Write the port map file
